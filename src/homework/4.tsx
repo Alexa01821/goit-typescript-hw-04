18be0542447d47b435c866bae787e231a69458d3
Alexa01821 <sahunia2004@gmail.com>
2023-11-11 22:11:26 +0200

    tasks done

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,28 +1,39 @@
-import React, { createContext, useMemo, useState, useContext } from "react";
+import React, {
+  createContext,
+  useMemo,
+  useState,
+  useContext,
+  ReactNode,
+} from "react";
 import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
-
-// Додати тип Menu Selected
+type SelectedMenu = {
+  id?: MenuIds;
+};
+type MenuSelected = { selectedMenu: SelectedMenu };
+type MenuAction = {
+  onSelectedMenu: (obj: SelectedMenu) => void;
+};
+type PropsProvider = {
+  children: ReactNode; // Додати тип для children
+};
+type PropsMenu = {
+  menus: Menu[]; // Додайте вірний тип для меню
+};
 
 const MenuSelectedContext = createContext<MenuSelected>({
   selectedMenu: {},
 });
 
-// Додайте тип MenuAction
-
 const MenuActionContext = createContext<MenuAction>({
   onSelectedMenu: noop,
 });
 
-type PropsProvider = {
-  children; // Додати тип для children
-};
-
 function MenuProvider({ children }: PropsProvider) {
-  // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] =
+    useState<SelectedMenu>({});
 
   const menuContextAction = useMemo(
     () => ({
@@ -40,16 +51,14 @@ function MenuProvider({ children }: PropsProvider) {
 
   return (
     <MenuActionContext.Provider value={menuContextAction}>
-      <MenuSelectedContext.Provider value={menuContextSelected}>
+      <MenuSelectedContext.Provider
+        value={menuContextSelected}
+      >
         {children}
       </MenuSelectedContext.Provider>
     </MenuActionContext.Provider>
   );
 }
-
-type PropsMenu = {
-  menus; // Додайте вірний тип для меню
-};
 
 function MenuComponent({ menus }: PropsMenu) {
   const { onSelectedMenu } = useContext(MenuActionContext);
@@ -58,9 +67,14 @@ function MenuComponent({ menus }: PropsMenu) {
   return (
     <>
       {menus.map((menu) => (
-        <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
-          {menu.title}{" "}
-          {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
+        <div
+          key={menu.id}
+          onClick={() => onSelectedMenu({ id: menu.id })}
+        >
+          {menu.title}
+          {selectedMenu.id === menu.id
+            ? "Selected"
+            : "Not selected"}
         </div>
       ))}
     </>
